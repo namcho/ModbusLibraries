5b9c3e519343f4228e978971afb7dcc4cb9dba08
namcho <nazimyildiz90@gmail.com>
2021-10-01 08:48:31 +0300

    MultiWrite ozelligi kaldirildi. Kullanici isterse adapter tarafinda bu islemi yapmali.

--- Modbus/ModbusPDU/modbus_pdu.h
+++ Modbus/ModbusPDU/modbus_pdu.h
@@ -12,13 +12,6 @@
 #include "../ModbusClientCommon/request_structure.h"
 #include "stdint.h"
 
-#define MULTIWRITE_BUFFERED_CONTEXT_LIM		2
-#define MULTIWRITE_CONTEXT_OFF	0
-#define MULTIWRITE_CONTEXT_ON	1
-#define CONTEXT_STATUS_FULL 		1
-#define CONTEXT_STATUS_EMPTY 		0
-#define CONTEXT_STATUS_NOTAVAIBLE	-1
-
 // Function Codes
 #define FN_CODE_READ_HOLDING				3
 #define FN_CODE_WRITE_SINGLE				6
@@ -42,27 +35,11 @@ typedef int16_t (*IReadParam)(uint16_t address);
 typedef int8_t (*IAddressCheck)(uint16_t address);
 
 
-struct MultiWriteBuffered{
-	uint16_t address_start;
-	uint16_t quantity_reg;
-
-	uint16_t head;
-	uint16_t tail;
-
-	int8_t status;			// dolu mu bos mu onu bildiriyor...
-	int16_t *buffer;		// register degerlerinin tutulacagi ram alani
-	uint16_t size;
-};
-
 typedef struct{
 	int8_t *buffer_tx;
 	int8_t *buffer_rx;
 
 	uint16_t response_size;		/*! Cevap paketinin kac byte ?*/
-
-	struct MultiWriteBuffered mWriteBuffered[MULTIWRITE_BUFFERED_CONTEXT_LIM];
-	uint8_t context;	// Hangi bufferin kullanilacagini bildirir
-	uint8_t multi_write_onoff;
 
 	int8_t (*IParamWrite)(uint16_t address, int16_t value);
 	int16_t (*IParamRead)(uint16_t address);
@@ -78,16 +55,7 @@ typedef struct{
  * @postcondition :
  */
 void ModbusPDUInit(ModbusPDU_t *pdu_obj, IWriteParam FuncWriteParam, IReadParam FuncReadParam, IAddressCheck FuncAddressCheck);
-/*
- * @brief         : Multiwrite fonksiyonunda yazim islemlerini RAM alanina yapip hizli bir sekilde client cihaza
- * 					cevap verebilmek amaciyla kullanilir(opsiyonel).
- * @param pdu_obj : Modbus Protocol islemleri icin kullanilan yapi.
- * @param context_no : Kucuk parcalar halinde birden fazla tampon alani tanimlamasi icin kullanilir. Tampob numarasini
- * 						belirtir.
- * @param buffer  : Kullanilacak RAM alani
- * @param size    :	RAM alani boyutu [byte]
- */
-void setModbusPDUMWBuffer(ModbusPDU_t *pdu_obj, uint8_t context_no, int16_t *buffer, uint16_t size);
+
 /*
  * @brief         :
  * @param[]       :
@@ -126,77 +94,5 @@ void RequestPacketWriteSingle(ModbusPDU_t *pdu_obj, ModbusRequest_t *request);
  */
 void RequestPacketWriteMultiple(ModbusPDU_t *pdu_obj, ModbusRequest_t *request);
 
-/*
- * @brief         :
- * @param[]       :
- * @return        :
- * @precondition  :
- * @postcondition :
- */
-void setMultiWriteContextOnOff(ModbusPDU_t *pdu_obj, uint8_t MULTIWRITE_CONTEXT_X);
-/*
- * @brief         :
- * @param[]       :
- * @return        :
- * @precondition  :
- * @postcondition :
- */
-uint16_t getMultiWriteContextValue(ModbusPDU_t *pdu_obj);
-/*
- * @brief         :
- * @param[]       :
- * @return        :
- * @precondition  :
- * @postcondition :
- */
-int8_t getMultiWriteContextStatus(ModbusPDU_t *pdu_obj, uint16_t context_no);
-/*
- * @brief         :
- * @param[]       :
- * @return        :
- * @precondition  :
- * @postcondition :
- */
-uint16_t getMultiWriteContextHead(ModbusPDU_t *pdu_obj, uint16_t context_no);
-/*
- * @brief         :
- * @param[]       :
- * @return        :
- * @precondition  :
- * @postcondition :
- */
-uint16_t getMultiWriteContextTail(ModbusPDU_t *pdu_obj, uint16_t context_no);
-/*
- * @brief         :
- * @param[]       :
- * @return        :
- * @precondition  :
- * @postcondition :
- */
-int16_t getMultiWriteContextData(ModbusPDU_t *pdu_obj, uint16_t context_no);
-/*
- * @brief         :
- * @param[]       :
- * @return        :
- * @precondition  :
- * @postcondition :
- */
-int16_t *getMultiWriteContextBuffer(ModbusPDU_t *pdu_obj, uint16_t context_no);
-/*
- * @brief         :
- * @param[]       :
- * @return        :
- * @precondition  :
- * @postcondition :
- */
-uint16_t getMultiWriteContextAddressStart(ModbusPDU_t *pdu_obj, uint16_t context_no);
-/*
- * @brief         :
- * @param[]       :
- * @return        :
- * @precondition  :
- * @postcondition :
- */
-uint16_t getMultiWriteContextQuantityRegister(ModbusPDU_t *pdu_obj, uint16_t context_no);
 
 #endif /* MODBUS_PDU_H_ */
