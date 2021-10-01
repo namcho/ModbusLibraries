218158806bfd52093907bf527eb78580a55142f3
namcho <nazimyildiz90@gmail.com>
2021-10-01 08:30:27 +0300

    Log header dosyasi senkronlandi.

--- Modbus/ModbusClientCommon/mc_log.h
+++ Modbus/ModbusClientCommon/mc_log.h
@@ -2,7 +2,7 @@
  * mc_log.h
  *
  *  Created on: 28Mar.,2018
- *      Author: EN
+ *      Author: Nazim Yildiz
  */
 
 #ifndef MODBUSCLIENTCOMMON_MC_LOG_H_
@@ -10,12 +10,12 @@
 #include "stdint.h"
 
 typedef enum{
-	eMC_LOG_ERRNO = 0,	/// hata yok
-	eMC_LOG_ERRCRC,		/// crc hatasi
+	eMC_LOG_ERRNO = 0,		/// hata yok
+	eMC_LOG_ERRCRC,			/// crc hatasi
 	eMC_LOG_ERRFUNC,		/// fonksiyon kodu hatasi
-	eMC_LOG_ERRTO,		   /// zaman asimi hatasi
-	eMC_LOG_ERRCONN,	  /// Parity, baudrate, ... hatasi
-	eMC_LOG_ERRDRV		/// Driver arayuzleri atanmamis
+	eMC_LOG_ERRTO,		   	/// zaman asimi hatasi
+	eMC_LOG_ERRCONN,	  	/// Parity, baudrate, ... hatasi
+	eMC_LOG_ERRDRV			/// Driver arayuzleri atanmamis
 }MCLogError_e;
 
 typedef struct{
@@ -24,7 +24,7 @@ typedef struct{
 	uint16_t start_adr;			/// islemin yapilacagi adres baslangici
 	int16_t quantity_reg;		/// islem yapilmak istenmis register miktari
 	uint16_t error_count;		/// ilgili paket icin toplam hata miktari
-	MCLogError_e error;		/// Hata hakkinda bilgi
+	MCLogError_e error;			/// Hata hakkinda bilgi
 }ModbusClientLogVars_t;
 
 typedef enum{
@@ -37,6 +37,8 @@ typedef struct{
 	uint16_t len;						/// *elements dizisinin boyutu
 	MCLogActive_e eMC_LOG_x;
 	uint16_t head;						/// Log islemi icin kullanilabilecek alanin indeksini belirtir
+
+	uint32_t err_total;
 }ModbusClientLog_t;
 
 /*
@@ -46,7 +48,7 @@ typedef struct{
  * @precondition
  * @postcondition
  */
-int8_t addMCL(ModbusClientLog_t *log_file, int8_t sa, int8_t funcno, uint16_t start_adr, uint16_t qtty_reg, MCLogError_e eMC_LOG_ERRx);
+int8_t addMCL(ModbusClientLog_t *MbLogObj, int8_t sa, int8_t funcno, uint16_t start_adr, uint16_t qtty_reg, MCLogError_e eMC_LOG_ERRx);
 /*
  * @brief
  * @param
@@ -54,7 +56,7 @@ int8_t addMCL(ModbusClientLog_t *log_file, int8_t sa, int8_t funcno, uint16_t st
  * @precondition
  * @postcondition
  */
-void setMCLActivation(ModbusClientLog_t *log_file, MCLogActive_e eMC_LOG_x);
+void setMCLActivation(ModbusClientLog_t *MbLogObj, MCLogActive_e eMC_LOG_x);
 /*
  * @brief
  * @param
@@ -62,7 +64,7 @@ void setMCLActivation(ModbusClientLog_t *log_file, MCLogActive_e eMC_LOG_x);
  * @precondition
  * @postcondition
  */
-int8_t getMCLBiggestErrorSlaveAdr(ModbusClientLog_t *log_file);
+int8_t getMCLBiggestErrorSlaveAdr(ModbusClientLog_t *MbLogObj);
 /*
  * @brief
  * @param
@@ -70,7 +72,7 @@ int8_t getMCLBiggestErrorSlaveAdr(ModbusClientLog_t *log_file);
  * @precondition
  * @postcondition
  */
-int8_t getMCLBiggestErrorFuncNo(ModbusClientLog_t *log_file);
+int8_t getMCLBiggestErrorFuncNo(ModbusClientLog_t *MbLogObj);
 /*
  * @brief
  * @param
@@ -78,7 +80,14 @@ int8_t getMCLBiggestErrorFuncNo(ModbusClientLog_t *log_file);
  * @precondition
  * @postcondition
  */
-uint16_t getMCLBiggestErrorQuantityReg(ModbusClientLog_t *log_file);
+uint16_t getMCLBiggestErrorQuantityReg(ModbusClientLog_t *MbLogObj);
+/*
+ * @brief Belirtilen modbus register adresi(start_adr) icin kac hata olusmus bilgisini iletir.
+ * @param MbLogObj
+ * @param start_adr Modbus start address bilgisidir. Not: Modbus Register Numarasiyla ayni degildir.
+ * @return Olusmus hata sayisi
+ */
+uint16_t getMCLStartAddressErrorCount(ModbusClientLog_t *MbLogObj, uint16_t start_adr);
 /*
  * @brief
  * @param
@@ -86,7 +95,24 @@ uint16_t getMCLBiggestErrorQuantityReg(ModbusClientLog_t *log_file);
  * @precondition
  * @postcondition
  */
-ModbusClientLogVars_t getMCLBiggestError(ModbusClientLog_t *log_file);
+ModbusClientLogVars_t getMCLBiggestError(ModbusClientLog_t *MbLogObj);
+/*
+ * @brief
+ * @param
+ * @return
+ * @precondition
+ * @postcondition
+ */
+void clearMCL(ModbusClientLog_t *MbLogObj);
+
+/*
+ * @brief
+ * @param
+ * @return
+ * @precondition
+ * @postcondition
+ */
+uint32_t getMCLTotalError(ModbusClientLog_t *MbLogObj);
 
 
 
